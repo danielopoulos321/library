28487ff6d77ba649f137705a78f9ab218ab871e4
Daniel Anestopoulos <dstops97@gmail.com>
2023-06-17 16:29:28 -0400

    Add Remove and Read/Unread Logic

---
+++ script.js
@@ -0,0 +1,75 @@
+let myLibrary = [];
+
+
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.info = function(){
+        return title + " by " + author + ", " + pages + " pages " + read;
+    }
+}
+
+Book.prototype.toggleRead = function() {
+    if(this.read == "Read"){
+        this.read = "Not Yet Read";
+    } else {
+        this.read = "Read";
+    }
+    displayBooks();
+}
+
+function addBookToLibrary() {
+    let title = document.getElementById('title').value;
+    let author = document.getElementById('author').value;
+    let pages = document.getElementById('pages').value;
+    let read = document.getElementById('read').checked ? "Read" : "Not Yet Read";
+    let book = new Book(title, author, pages, read);
+    myLibrary.push(book);
+}
+
+function displayBooks() {
+    let container = document.getElementById("container");
+    container.innerHTML = "";
+  
+    for (let i = 0; i < myLibrary.length; i++) {
+      let card = document.createElement("div");
+      card.dataset.indexNumber = i;
+  
+      let content = `
+          <h2>${myLibrary[i].title}</h2>
+          <h3>By ${myLibrary[i].author}</h3>
+          <p>${myLibrary[i].pages} pages</p>
+          <button onclick="myLibrary[${i}].toggleRead()" >${myLibrary[i].read}</button>
+          <button onclick="removeBook(${i})" >Remove Book</button>
+      `;
+      card.innerHTML = content;
+      container.appendChild(card);
+    }
+  }
+
+  function removeBook(index){
+    myLibrary.splice(index, index+1);
+    displayBooks();
+  }
+
+const bookForm = document.getElementById('newBook');
+bookForm.addEventListener("submit", function(event) {
+    addBookToLibrary();
+    displayBooks();
+    event.preventDefault();
+    })
+
+function showForm(){
+      bookForm.removeAttribute('hidden');
+      //Update this to cancel out of adding a book
+}      
+
+const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', '295', 'Not Yet Read');
+const bible = new Book('The Bible', 'God', '1440', 'Read');
+const nineteen84 = new Book('1984', 'George Orwell', '888', 'Not Yet Read');
+myLibrary.push(theHobbit);
+myLibrary.push(bible);
+myLibrary.push(nineteen84);
+displayBooks();
